853c29c266a8dae9db2d4b1daab5f7da0149864e
Martin Georgiev <mgeorgiev9611@abv.bg>
2020-04-15 07:06:55 +0100

    Updates

---
+++ precache-manifest.c169a5a5a7a042f75d76bcb910ab1331.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03004b9c1bd5c0e97d70558456afaebf",
+    "url": "/Weather/index.html"
+  },
+  {
+    "revision": "86bb1d838fbc45782c6c",
+    "url": "/Weather/static/css/main.f077cf93.chunk.css"
+  },
+  {
+    "revision": "80c10d925fc4d5f34f37",
+    "url": "/Weather/static/js/2.308effbe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Weather/static/js/2.308effbe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86bb1d838fbc45782c6c",
+    "url": "/Weather/static/js/main.5b585ea5.chunk.js"
+  },
+  {
+    "revision": "00a831ffb96062f9b232",
+    "url": "/Weather/static/js/runtime-main.f4664cab.js"
+  }
+]);
